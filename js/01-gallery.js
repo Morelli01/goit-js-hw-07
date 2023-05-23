ff50297b0b17333f1122f2135a5364898fae1d96
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-05-23 22:20:02 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryItems = [
+  {
+    preview: 'small-image-1.jpg',
+    original: 'large-image-1.jpg',
+    description: 'Image 1',
+  },
+  {
+    preview: 'small-image-2.jpg',
+    original: 'large-image-2.jpg',
+    description: 'Image 2',
+  },
+  {
+    preview: 'small-image-3.jpg',
+    original: 'large-image-3.jpg',
+    description: 'Image 3',
+  },
+];
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', handleGalleryItemClick);
+
+function handleGalleryItemClick(event) {
+  event.preventDefault();
+  
+  const target = event.target;
+  if (target.classList.contains('gallery__image')) {
+    const imageSource = target.dataset.source;
+    openModal(imageSource);
+  }
+}
+
+function openModal(imageSource) {
+  const modal = basicLightbox.create(`
+    <img src="${imageSource}" width="800" height="600">
+  `);
+
+  modal.show();
+
+  document.addEventListener('keydown', closeModalOnEscape);
+
+  function closeModalOnEscape(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      document.removeEventListener('keydown', closeModalOnEscape);
+    }
+  }
+}
